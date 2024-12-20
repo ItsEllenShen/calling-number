c776bb0160a3bd99b4ce5ba209af7092cfd683b3
ItsEllenShen <159000201+ItsEllenShen@users.noreply.github.com>
2024-12-20 09:03:17 +0800

    Add files via upload

---
+++ staff.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("numberInput");
+const numberButtons = document.querySelectorAll(".number");
+const enterButton = document.querySelector(".enter");
+const deleteButton = document.querySelector(".delete");
+const sound = document.getElementById("dingSound");
+
+let currentNumber = "";
+
+numberButtons.forEach(button => {
+    button.addEventListener("touchstart", event => {
+        event.preventDefault();
+      const value = button.dataset.value; // 取得按鈕的數字
+      currentNumber += value; // 將數字加到 currentNumber
+      input.value = currentNumber; // 更新輸入框顯示
+    });
+  });
+
+deleteButton.addEventListener("touchstart", event => {
+    event.preventDefault();
+currentNumber = currentNumber.slice(0, -1); // 刪除最後一個字元
+input.value = currentNumber; // 更新輸入框顯示
+});
+
+enterButton.addEventListener("touchstart", event => {
+    event.preventDefault();
+    if (currentNumber) { // 確保欄位不是空的
+        sound.play();
+        localStorage.setItem("currentNumber", number);
+        const message = `請取餐編號 ${currentNumber} 客人取餐 謝謝`;
+        speak(message);
+        currentNumber = ""; // 清空數字
+        input.value = ""; // 清空輸入框
+    } else {
+      alert("請輸入取餐編號！");
+    }
+  });
